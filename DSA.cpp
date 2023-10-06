ad8bd82ff8928c40bd287b7c3b932f80911a31cd
ZIYA ALI <147191091+ziyyaa1@users.noreply.github.com>
2023-10-06 23:33:49 +0500

    Add files via upload

---
+++ DSA.cpp
@@ -0,0 +1,39 @@
+//C++ FROM SCRATCH
+//semester 1, semester 2, semester 3.
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	//DAY 1:
+//  printing
+//  data types (char, string, int, float, double), Typecasting.
+//  operators (arithmetic, logical, relational), div rule.
+//  Loops (if, if-else, while, for), switch case.
+	int i = 1, n;
+	cout << "Enter number of rows you want to print\n";
+	/*int AGE;
+	char favletter;
+	string name;
+	float height;
+	cout<<"Enter your name:\n ";
+	//cin>> donot recognises spaces so use getline() along with string library
+	getline(cin,name);
+	cout << "Enter your AGE, height, favletter:\n ";
+	cin >> AGE >> height >> favletter;
+	cout << "Your name is " << name << "\nYour AGE is " << AGE << "\nYour height is " << height << "\n";
+	cout << "Size of your height: " << sizeof(height);*/
+}			
+			
+//SHORTCUTS FOR VS:
+//1. select complete line by clicking line number.
+//2. ctrl+c & ctrl+v, line will be pasted below that line.
+//3. ctrl+x for cutting line.
+//4. ctrl+d for duplicate lines and write.
+//5. ctrl+home to go at start
+//6. ctrl+end to go at end.
+//7. ctrl+shit+H for replacing word by new.
+//8. alt+arrow buttons to move lines.
+//9. shift+alt+arrow buttons to write on multiple lines.
+//10. shift+alt+; for editing a word in whole program.
